d145842414ea2b915377b0a51a659917499a70c6
Julian Garcia Duarte <juliangarciaduarte@gmail.com>
2023-05-16 21:59:35 -0500

    Corrige errores de MPI

--- implementacion_mpi/mpi_summaSimple.c
+++ implementacion_mpi/mpi_summaSimple.c
@@ -2,57 +2,42 @@
 // Modified by C. Barrios for training purposes 2023
 // Simple Program to calculate the sum of first n natural numbers
 // Positive integers 1,2,3...n are known as natural numbers
-
 #include <stdio.h>
 #include <mpi.h>
 
-int main(int argc, char **argv[])
-{
-    FILE *saving_file;
-    int num, count, globalSum= 0;
-    double exec_time, start_timer, end_timer;
-    
-    // Asks for the number
-    printf("Digite un numero entero positivo ");
-    scanf("%d", &num);
-
-    //Starts running time
-    start_timer=omp_get_wtime();
-
+int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
 
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int localSum = 0;
-    int start = rank * (num / size) + 1;  // Calculate the starting number for each process
-    int end = (rank + 1) * (num / size);  // Calculate the ending number for each process
+    double startTime = MPI_Wtime();
 
-    for (count = start; count <= end; count++) {
+    int localSum = 0;
+    int start = rank * (100 / size) + 1;  // Calculate the starting number for each process
+    int end = (rank + 1) * (100 / size);  // Calculate the ending number for each process
+
+    for (int i = start; i <= end; i++) {
         localSum += i;  // Calculate the local sum for each process
     }
 
+    int globalSum = 0;
     MPI_Reduce(&localSum, &globalSum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
+    double endTime = MPI_Wtime();
+
     if (rank == 0) {
-        printf("Sum: %d\n", globalSum);
+        FILE* file = fopen("result.txt", "w");
+        if (file != NULL) {
+            fprintf(file, "Sum: %d\n", globalSum);
+            fprintf(file, "Execution time: %f seconds\n", endTime - startTime);
+            fclose(file);
+        } else {
+            printf("Error opening the file for writing.\n");
+        }
     }
 
     MPI_Finalize();
-
-    // Ends time
-    end_timer=omp_get_wtime();
-    exec_time=(end_timer - start_timer);
-
-    printf("\nSum = %d\n", sum);
-
-    // Saves the results into a text file
-    saving_file=fopen("output_summaSimple.txt", "w");
-    fprintf(saving_file, "Calculo de la suma de los %d primero numeros naturales", num);
-    fprintf(saving_file, "\nResultado: %d", sum);
-    fprintf(saving_file, "\nEl tiempo de ejecucion del procedimiento fue: %d microsegundos\n", (int) (exec_time*1000000));
-    fclose(saving_file);
-
     return 0;
 }
